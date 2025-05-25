f548112f5ff9ed29c780b9ee83865b00b2ee8faf
MounikaB2004 <mounibendi04@gmail.com>
2025-05-25 17:22:37 +0530

    Add files via upload

---
+++ MaxElement.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class MaxElement{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        sc.close();
+        int max=a[0];  // initialize max as starting element
+        for(int i=0;i<a.length;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        System.out.print(max);
+    }
+}
