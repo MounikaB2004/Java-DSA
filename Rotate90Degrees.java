be227af67be90202d5abc359dab2454ec444beda
MounikaB2004 <mounibendi04@gmail.com>
2025-06-13 11:49:43 +0530

    Add files via upload

---
+++ Rotate90Degrees.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Rotate90Degrees {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        //Transpose
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp=a[i][j];
+                a[i][j]=a[j][i];
+                a[j][i]=temp;
+            }
+        }
+        //Reversing rows
+        for(int i=0;i<n;i++){
+            int left=0;
+            int right=n-1;
+            while(left<=right){
+                int temp=a[i][left];
+                a[i][left]=a[i][right];
+                a[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+        System.out.println();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+3
+1 2 3
+4 5 6
+7 8 9
+
+7 4 1
+8 5 2
+9 6 3
+*/
