982194e666db0da0b9e7f7797b18096ccf207254
MounikaB2004 <mounibendi04@gmail.com>
2025-07-15 10:07:21 +0530

    Add files via upload

---
+++ DeleteHeadNode.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class DeleteHeadNode{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        LL list=new LL();
+        for(int i=0;i<n;i++){
+            int val=sc.nextInt();
+            list.insert(val);
+        }
+        list.deleteHead();
+        list.display();
+    }
+}
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+class LL{
+    Node head;
+    void insert(int data){
+        Node newn=new Node(data);
+        if(head==null){
+            head=newn;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newn;
+    }
+    void deleteHead(){
+        if(head==null){
+            System.out.print("Nothing to delete");
+        }else{
+            head=head.next;
+        }
+    }
+    void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.print("null");
+    }
+}
+/*
+5
+34 67 89 90 23
+67->89->90->23->null
+*/
