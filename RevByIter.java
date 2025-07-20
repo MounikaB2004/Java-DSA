2cd08ca5157527dc5d991c43f602b89ffd9cb80a
MounikaB2004 <mounibendi04@gmail.com>
2025-07-20 13:37:35 +0530

    Add files via upload

---
+++ RevByIter.java
@@ -0,0 +1,66 @@
+import java.util.*;
+public class RevByIter {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        LL list=new LL();
+        for(int i=0;i<n;i++){
+            int val=sc.nextInt();
+            list.insert(val);
+        }
+        System.out.println("reversed list:");
+        list.revIter();
+        list.display();
+    }
+}
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+class LL{
+    Node head;
+    void insert(int data){
+        Node newn=new Node(data);
+        if(head==null){
+            head=newn;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newn;
+    }
+
+    void revIter(){
+        Node prev=null;
+        Node curr=head;
+        Node next=null;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+    }
+
+    void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.print("null");
+    }
+}
+/*
+6
+23 56 89 34 12 19
+reversed list:
+19->12->34->89->56->23->null
+*/
